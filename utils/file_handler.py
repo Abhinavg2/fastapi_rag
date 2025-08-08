facf68481986e597ab438c961511e6cb66d9397e
Abhinavg2 <118332556+Abhinavg2@users.noreply.github.com>
2025-08-08 11:52:05 +0530

    Delete utils directory

--- utils/file_handler.py
+++
@@ -1,8 +0,0 @@
-import os, shutil
-
-async def save_uploaded_file(file, folder="uploaded_files"):
-    os.makedirs(folder, exist_ok=True)
-    path = os.path.join(folder, file.filename)
-    with open(path, "wb") as f:
-        shutil.copyfileobj(file.file, f)
-    return path
